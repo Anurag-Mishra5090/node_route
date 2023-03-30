9f5d5d3649fa13093b60f015ade7230f382f4495
= <=>
2023-03-30 11:32:11 +0530

    node

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const port=8008;
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.write("Anurag Page");
+        res.end()
+    }
+
+    else if(req.method==="GET" && req.url === '/about'){
+        res.write("Got It");
+        res.end();
+    }
+    else if(req.url === "/contact"){
+        res.write("Contact Us");
+        res.end();
+
+    }
+    
+})
+
+server.listen(port)
+console.log('Hello')
+
+
